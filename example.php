45e6594c7c415d9dca87a0159ff74ce981a17059
HugoDerave <30510534+HugoDerave@users.noreply.github.com>
2019-03-15 16:46:35 +0100

    Create example.php

---
+++ example.php
@@ -0,0 +1,19 @@
+<?php
+//Hasn't been tested but should be working
+
+$apikey = '';
+$platform = 'PC';
+$player = 'HeyImLIFELINE';
+
+$ch = curl_init();
+
+curl_setopt($ch, CURLOPT_URL, 'http://api.apexlegendsstatus.com/bridge?platform='.$platform.'&player='.$player.'&auth='.$apikey);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'Error:' . curl_error($ch);
+}
+curl_close ($ch);
+
+print_r($result);
